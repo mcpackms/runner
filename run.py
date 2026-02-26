2689c2c6f38292736dfb016076c0c17c3a647427
wang <mcpackms@outlook.com>
2026-02-26 08:55:11 +0800

    c2

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ from datetime import datetime
 # 配置 - 针对 GitHub Actions 优化
 BASE_URL = "http://jinshan2.resource.zhibaowan.com/downbag2/XT/2017/app/app-%s.apk"
 DOWNLOAD_DIR = os.getenv('GITHUB_WORKSPACE', os.getcwd())  # 使用GitHub工作区或当前目录
-START_NUM = 7500
-END_NUM = 7990
+START_NUM = 7000
+END_NUM = 7490
 TOTAL_FILES = END_NUM - START_NUM + 10
 MAX_WORKERS = 4
 
